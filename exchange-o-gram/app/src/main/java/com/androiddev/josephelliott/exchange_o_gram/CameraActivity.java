28a48bba51150300f2910099d38633fd81406da9
etbrady <etbrady@outlook.com>
2016-01-24 23:49:17 -0600

    Fixed head

---
+++ exchange-o-gram/app/src/main/java/com/androiddev/josephelliott/exchange_o_gram/CameraActivity.java
@@ -0,0 +1,18 @@
+package com.androiddev.josephelliott.exchange_o_gram;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Joseph Elliott on 1/24/2016.
+ */
+public class CameraActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
