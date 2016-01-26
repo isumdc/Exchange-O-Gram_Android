a61e1610c28b11ee16660b4a1df90a5fea90d898
Joseph Elliott <joey.w.elliott@gmail.com>
2016-01-25 18:35:31 -0600

    updated manifest

--- exchange-o-gram/app/src/main/java/com/androiddev/josephelliott/exchange_o_gram/CameraActivity.java
+++ exchange-o-gram/app/src/main/java/com/androiddev/josephelliott/exchange_o_gram/CameraActivity.java
@@ -2,6 +2,7 @@ package com.androiddev.josephelliott.exchange_o_gram;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by Joseph Elliott on 1/24/2016.
@@ -22,6 +23,10 @@ public class CameraActivity extends Activity {
         //CaptureRequest, CaptureRequest.CameraBuilder -> Link Surface for Viewing -> Make CaptureRequest
 
         //CameraCaptureSession -> Capture Camare Image and put the result on the Surface registered in CaptureRequest.
+
+    }
+
+    public void takePictureIntent(View v) {
 
     }
 
