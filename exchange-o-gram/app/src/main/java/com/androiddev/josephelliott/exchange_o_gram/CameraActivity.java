64b14f4a4aed286728c6149fe7e1d00eb60acb25
Joseph Elliott <joey.w.elliott@gmail.com>
2016-01-25 18:41:36 -0600

    removed .gitignore

--- exchange-o-gram/app/src/main/java/com/androiddev/josephelliott/exchange_o_gram/CameraActivity.java
+++ exchange-o-gram/app/src/main/java/com/androiddev/josephelliott/exchange_o_gram/CameraActivity.java
@@ -2,33 +2,46 @@ package com.androiddev.josephelliott.exchange_o_gram;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 
 /**
  * Created by Joseph Elliott on 1/24/2016.
  */
 public class CameraActivity extends Activity {
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private ImageView mImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-
-        // Camera2 api is garbage and infinitely more confusing than necessary.
-
-        //CameraManager -> Select Camera -> Create CameraDevice
-
-        //CameraDevice -> Create CaptureRequest -> Create CameraCaptureSession
-
-        //CaptureRequest, CaptureRequest.CameraBuilder -> Link Surface for Viewing -> Make CaptureRequest
-
-        //CameraCaptureSession -> Capture Camare Image and put the result on the Surface registered in CaptureRequest.
-
+        mImageView = (ImageView) findViewById(R.id.camera_preview);
     }
 
     public void takePictureIntent(View v) {
-        Intent intent = new Intent();
+        dispatchTakePictureIntent();
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
     }
 
 }
