eb15b7bcbf419e9352f44b998a239c75dfdc8eb3
Joseph Elliott <joey.w.elliott@gmail.com>
2016-01-26 00:29:28 -0600

    camera and gallery work for getting images

--- exchange-o-gram/app/src/main/java/com/androiddev/josephelliott/exchange_o_gram/CameraActivity.java
+++ exchange-o-gram/app/src/main/java/com/androiddev/josephelliott/exchange_o_gram/CameraActivity.java
@@ -3,19 +3,25 @@ package com.androiddev.josephelliott.exchange_o_gram;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by Joseph Elliott on 1/24/2016.
  */
 public class CameraActivity extends Activity {
-
-    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     private ImageView mImageView;
 
@@ -26,24 +32,53 @@ public class CameraActivity extends Activity {
         mImageView = (ImageView) findViewById(R.id.camera_preview);
     }
 
-    public void takePictureIntent(View v) {
-        dispatchTakePictureIntent();
+    /**
+     * Uploads the image to the server
+     * */
+    public void uploadImage(View v) {
+        // Just get the image from mImageView and upload it to the server
     }
 
-    // TODO This crashes because of a security exception
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
+    /**
+     * Method for handling getting the picture from the camera
+     * Holy cow. http://developer.android.com/reference/android/provider/MediaStore.html#ACTION_IMAGE_CAPTURE
+     * Notice how it says that IF YOU HAVE THE CAMERA PERMISSION YOU'LL CRASH THE APP. How unintuitive.
+     * I had to remove the camera permission for this app to get the camera intent to work. Ridiculous.
+     * */
+    public void fromCamera(View v) {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, 1);
+    }
+
+    /**
+     * Method for handling getting the picture from the user's storage
+     * */
+    public void fromGallery(View v) {
+        Intent intent = new Intent();
+        // Show only images, no videos or anything else
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        // Always show the chooser (if there are multiple options available)
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), 2);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            mImageView.setImageBitmap(imageBitmap);
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 2 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            // Get the uri which is like content://media.whatever...
+            Uri uri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                mImageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            mImageView.setImageBitmap(photo);
         }
     }
 
